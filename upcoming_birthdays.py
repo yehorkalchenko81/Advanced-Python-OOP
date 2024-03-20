7703d764988377f58dbe3404d7289e59b0a817a4
Yehor Kalchenko <108671736+yehorkalchenko81@users.noreply.github.com>
2024-03-20 17:33:13 +0000

    Update upcoming_birthdays.py

--- upcoming_birthdays.py
+++ upcoming_birthdays.py
@@ -10,17 +10,18 @@ def get_upcoming_birthdays(users: list) -> tuple:
         name, date_of_birth = list(user.values())
         date_time_bd = datetime.strptime(date_of_birth, '%Y.%m.%d')
         birthday = datetime(year=today.year, month=date_time_bd.month, day=date_time_bd.day)
+        res = birthday.timetuple().tm_yday - today.timetuple().tm_yday
 
         if birthday.weekday() >= 5:
             birthday += timedelta(days=2) if birthday.weekday() == 5 else timedelta(days=1)
 
-        if birthday.timetuple().tm_yday - today.timetuple().tm_yday in range(0, 7):
+        if res in range(0, 7):
             upcoming_birthdays.append({
                 'name': name,
                 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')
             })
 
-        elif birthday.timetuple().tm_yday - today.timetuple().tm_yday > 7:
+        elif res > 7:
             birthdays.append({
                 'name': name,
                 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')
