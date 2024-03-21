5cea1440b48b00e74070b22324d20859c27d8200
Yehor Kalchenko <108671736+yehorkalchenko81@users.noreply.github.com>
2024-03-21 11:42:45 +0000

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+from classes import Record
+
+
+def command_add(args, book):
+    if len(args) < 2:
+        return 'Not enough values for this commad!'
+    contact = Record(args[0])
+    contact.add_phone(args[1])
+    book.add_record(contact)
+    return f'{args[0]}\'s contact has been added'
